aeb353b10e9661aaeb11fe1e7a878215f07ff1e8
DevIzu <izharkhurshid3964@gmail.com>
2025-11-21 23:43:31 +0500

    Fix TS error by correctly renaming destructured 'mode' property to '_mode'

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
-import { defineConfig, loadEnv } from 'vite' // loadEnv is still needed
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  // The following line caused the error because the calculated variable was not used.
-  // const _env = loadEnv(mode, '.', ''); 
+export default defineConfig(({ mode: _mode }) => { 
   
-  // We don't need to load env variables here if they aren't used in this config.
-
   return {
     plugins: [react()],
     // Define global for libraries that rely on it (like html-docx-js)
